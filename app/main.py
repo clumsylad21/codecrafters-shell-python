9454ec07837c4059864a55ded782f854eb832133
Bhavyansh Sharma <jayashsharma4@gmail.com>
2026-06-19 08:17:13 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,47 +1,66 @@
 import sys
 import os
+import subprocess
 
 BUILTINS = ["echo", "exit", "type"]
 
-def run_command(cmd, arg):
+
+def find_executable(cmd):
+    for directory in os.environ.get("PATH", "").split(os.pathsep):
+        full_path = os.path.join(directory, cmd)
+
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            return full_path
+
+    return None
+
+
+def run_command(cmd, args):
     if cmd == "echo":
-        print(" ".join(arg))
+        print(" ".join(args))
+
     elif cmd == "exit":
         sys.exit(0)
+
     elif cmd == "type":
-        target = arg[0] if arg else None
+        target = args[0] if args else None
+
         if target in BUILTINS:
             print(f"{target} is a shell builtin")
             return
+
+        executable = find_executable(target)
+
+        if executable:
+            print(f"{target} is {executable}")
         else:
-            for directory in os.environ.get("PATH", "").split(os.pathsep):
-                full_path = os.path.join(directory, target)
-                if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-                    print(f"{target} is {full_path}")
-                    return
-                
-        print(f"{target} not found")
+            print(f"{target}: not found")
+
     else:
-        print(f"{cmd}: command not found")
+        executable = find_executable(cmd)
+
+        if executable:
+            subprocess.run([executable] + args)
+        else:
+            print(f"{cmd}: command not found")
+
 
 def main():
-
     while True:
-     sys.stdout.write("$ ")
-     sys.stdout.flush()
-    
-     user_input = sys.stdin.readline().strip()
-     parts = user_input.split()
-      
-     if not parts:
-        continue
-     
-     cmd = parts[0]
-     args = parts[1:]
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-     run_command(cmd, args)
+        user_input = sys.stdin.readline().strip()
+        parts = user_input.split()
 
-     
+        if not parts:
+            continue
+
+        cmd = parts[0]
+        args = parts[1:]
+
+        run_command(cmd, args)
+
 
 if __name__ == "__main__":
     main()
